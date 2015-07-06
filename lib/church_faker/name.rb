df2f8d42e220867ba3fcb07d9abb0bc68a90b935
Jesse J. Anderson <jessejanderson@gmail.com>
2015-07-05 20:52:46 -0700

    add CHURCH_TYPES and additional name case
    church is defined so that the more common "Church" is still used at least half the time

--- lib/church_faker/name.rb
+++ lib/church_faker/name.rb
@@ -4,12 +4,13 @@ module FFaker
     extend self
 
     def name
-      case rand(5)
-      when 0 then "#{number} Church of #{city}"
-      when 1 then "#{number} #{denomination} Church of #{city}"
-      when 2 then "#{city} #{denomination}"
-      when 3 then "#{city} Church"
-      when 4 then "#{cheese_name}"
+      case rand(6)
+        when 0 then "#{number} Church of #{city}"
+        when 1 then "#{number} #{denomination} Church of #{city}"
+        when 2 then "#{city} #{denomination}"
+        when 3 then "#{city} #{church}"
+        when 4 then "#{cheese_name}"
+        when 5 then "#{city} #{denomination} #{church}"
       end
     end
 
@@ -30,8 +31,16 @@ module FFaker
       DENOMINATIONS.sample
     end
 
+    def church
+      case rand(2)
+        when 0 then CHURCH_TYPES.sample
+        when 1 then "Church"
+      end
+    end
+
     NUMBERS = k %w[First Second Third Last Forty-Second] unless const_defined? :NUMBERS
     CHEESE_NAMES = k %w[Churchopolis Crosspointe Elevate Encounter Insurgence Kinetic Pulse Recreate Reproduce Resurgence StarChurch TheConnectingPoint TheCore Veritas Verve Victory] unless const_defined? :CHEESE_NAMES
     DENOMINATIONS = k %w[Baptist Catholic Charismatic Christian Evangelical Foursquare Lutheran Pentecostal Presbyterian Reformed United] unless const_defined? :DENOMINATIONS
+    CHURCH_TYPES = k %w[Assembly Chapel Christian\ Center Church Fellowship Ministries Parish Tabernacle] unless const_defined? :CHURCH_TYPES
   end
 end
